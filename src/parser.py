76833c4d8f4de50ab8101766d796dbb746f35ccf
Irek Iunusov <iiunusov@1-B-2.42heilbronn.de>
2026-06-11 11:17:38 +0200

    config and parser separately

--- src/parser.py
+++ src/parser.py
@@ -1,28 +1,8 @@
 import sys
 import json
-from typing import Dict, Any, List
-from pydantic import BaseModel, ValidationError, Field
-from pydantic import PositiveInt, NonNegativeInt
-
-
-class Level(BaseModel):
-    number: PositiveInt
-    width: PositiveInt = 14
-    height: PositiveInt = 12
-    seed: int = 42
-    level_max_time: PositiveInt = 90
-
-
-class Config(BaseModel):
-    highscore_filename: str = Field(min_length=1)
-    level: List[Level]
-    lives: PositiveInt = 3
-    pacgum: NonNegativeInt = 42
-    points_per_pacgum: PositiveInt = 10
-    points_per_super_pacgum: PositiveInt = 50
-    points_per_ghost: PositiveInt = 200
-    seed: int = 42
-    cheat: bool = False
+from typing import Dict, Any
+from pydantic import ValidationError
+from src.config import Config
 
 
 class Parser:
