26a2474bd8908a9ac0fbae7bc507138c95de3629
Irek Iunusov <iiunusov@1-B-2.42heilbronn.de>
2026-06-11 11:06:51 +0200

    parser improved

--- src/parser.py
+++ src/parser.py
@@ -1,54 +1,51 @@
+import sys
 import json
-from typing import Dict, Any
-from pydantic import BaseModel, Field, ValidationError, PositiveInt
-from typing import List, Dict, Any
+from typing import Dict, Any, List
+from pydantic import BaseModel, ValidationError, Field
+from pydantic import PositiveInt, NonNegativeInt
+
+
+class Level(BaseModel):
+    number: PositiveInt
+    width: PositiveInt = 14
+    height: PositiveInt = 12
+    seed: int = 42
+    level_max_time: PositiveInt = 90
+
 
 class Config(BaseModel):
     highscore_filename: str = Field(min_length=1)
-    level: List[Dict[str, Any]]
+    level: List[Level]
     lives: PositiveInt = 3
-    pacgum: PositiveInt = 42
+    pacgum: NonNegativeInt = 42
     points_per_pacgum: PositiveInt = 10
     points_per_super_pacgum: PositiveInt = 50
     points_per_ghost: PositiveInt = 200
     seed: int = 42
-    level_max_time: PositiveInt = 90
     cheat: bool = False
 
 
 class Parser:
-    @staticmethod
-    def delete_comments(text: List[str]) -> List[str]:
-        return [line for line in text if not line.strip().startswith("#")]
-        # for line in text:
-        #     if not line.strip().startswith("#"):
-        #         result.append(line)
-
     @classmethod
-    def get_config_data(cls, path: str) -> Dict[str, Any]:
-        cfg_data: Dict[str, Any] = {}
-        with open(path) as f:
-            raw_text = f.readlines()
-            clean_json = cls.delete_comments(raw_text)
-            try:
-                cfg_data = json.loads("".join(clean_json))
-            except json.JSONDecodeError as e:
-                print("Got JSON decoding error:", e)
-        return cfg_data
+    def _get_config_data(cls, path: str) -> Dict[str, Any]:
+        try:
+            with open(path) as f:
+                raw_text = f.readlines()
+                clean_json = [line for line in raw_text
+                            if not line.strip().startswith("#")]
+                return json.loads("".join(clean_json))
+        except json.JSONDecodeError as e:
+            print("\nGot JSON decoding error:", e)
+        except FileNotFoundError as e:
+            print(f"\nConfig file not found: {path}")
+        sys.exit(1)
     
     @classmethod
     def get_config(cls, path: str) -> Config:
-        cfg_data = cls.get_config_data(path)
-
-        if not cfg_data:
-            print("Could not fetch config data")
-            return None
+        cfg_data = cls._get_config_data(path)
 
         try:
-            config = Config.model_validate(cfg_data)
-            return config
+            return Config.model_validate(cfg_data)
         except ValidationError as e:
-            print(f"Got an error:", e)
-
-
-
+            print(f"\nGot an error:", e)
+            sys.exit(1)
