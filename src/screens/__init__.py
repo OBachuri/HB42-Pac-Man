711eccce4d4466ebc3438a87acdf36914b55deeb
Irek Iunusov <iiunusov@1-B-31.42heilbronn.de>
2026-06-16 16:03:09 +0200

    screens is a package. main_menu updated

---
+++ src/screens/__init__.py
@@ -0,0 +1,6 @@
+from .screen_types import ScreenTypes
+from .base_screen import BaseScreen
+from .main_menu import MainMenuScreen
+from .instructions_screen import InstructionsScreen
+
+__all__ = ["ScreenTypes", "BaseScreen", "MainMenuScreen", "InstructionsScreen"]
--- src/screens/main_menu.py
+++ src/screens/main_menu.py
@@ -5,21 +5,21 @@ if TYPE_CHECKING:
     from src.app import App
 
 from src.constants import *
-from src.screens.base_screen import BaseScreen
-from src.screens.screen_types import ScreenTypes
-
-screen = None
-clock = None
-pacman_icon = None
-font_large = None
-font_medium = None
-font_small = None
+from src.screens import BaseScreen, ScreenTypes
 
 
 class Button:
-    def __init__(self, x, y, width, height, text):
+    def __init__(
+            self,
+            y,
+            width,
+            text,
+            icon: pg.Surface,
+            x = SCREEN_WIDTH//2 - 75,
+            height = 50) -> None:
         self.rect = pg.Rect(x, y, width, height)
         self.text = text
+        self.icon = icon
         self.hovered = False
         self.selected = False
 
@@ -28,13 +28,13 @@ class Button:
         pg.draw.rect(surface, "yellow", self.rect, 2)
 
         # Draw image on the left if selected
-        if self.selected:
-            img_rect = pacman_icon.get_rect(
+        if self.selected or self.hovered:
+            img_rect = self.icon.get_rect(
                 center=(self.rect.left - 40, self.rect.centery))
-            surface.blit(pacman_icon, img_rect)
+            surface.blit(self.icon, img_rect)
 
         # Draw text
-        text_surf = font_small.render(self.text, True, "yellow")
+        text_surf = pg.font.Font(None, 30).render(self.text, True, "yellow")
         text_rect = text_surf.get_rect(center=self.rect.center)
         surface.blit(text_surf, text_rect)
 
@@ -51,27 +51,17 @@ class MainMenuScreen(BaseScreen):
         self.app = app
 
     async def run(self) -> None:
-        global screen
-        global clock
-        global pacman_icon
-        global font_large
-        global font_medium
-        global font_small
-
-        screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen = self.app.screen
         pg.display.set_caption("Pac-Man")
         clock = pg.time.Clock()
         pacman_icon = pg.image.load(
             "src/inc/img/pacman/stay/S01.png").convert_alpha()
-        font_large = pg.font.Font(None, 80)
-        font_medium = pg.font.Font(None, 40)
-        font_small = pg.font.Font(None, 30)
 
         buttons = [
-            Button(SCREEN_WIDTH//2 - 75, 200, 150, 50, "START GAME"),
-            Button(SCREEN_WIDTH//2 - 75, 300, 220, 50, "VIEW HIGHSCORES"),
-            Button(SCREEN_WIDTH//2 - 75, 400, 200, 50, "INSTRUCTIONS"),
-            Button(SCREEN_WIDTH//2 - 75, 500, 150, 50, "EXIT"),
+            Button(y=200, width=150, text="START GAME", icon=pacman_icon),
+            Button(y=300, width=220, text="VIEW HIGHSCORES", icon=pacman_icon),
+            Button(y=400, width=200, text="INSTRUCTIONS", icon=pacman_icon),
+            Button(y=500, width=150, text="EXIT", icon=pacman_icon)
         ]
 
         selected_index = 0
@@ -84,7 +74,6 @@ class MainMenuScreen(BaseScreen):
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    # running = False
                     self.app.quit()
 
                 if event.type == pg.KEYDOWN:
@@ -101,20 +90,17 @@ class MainMenuScreen(BaseScreen):
                     elif event.key == pg.K_RETURN or event.key == pg.K_SPACE:
                         selected_button = buttons[selected_index]
                         if selected_button.text == "START GAME":
-                            # print("Starting game...")
-                            # Call game function here
                             self.app.move_to(ScreenTypes.GAME)
                             running = False
                         elif selected_button.text == "VIEW HIGHSCORES":
                             print("Opening highscores...")
                         elif selected_button.text == "INSTRUCTIONS":
-                            print("Opening instructions...")
+                            self.app.move_to(ScreenTypes.INSTRUCTIONS)
+                            running = False
                         elif selected_button.text == "EXIT":
-                            # running = False
                             self.app.quit()
 
                     elif event.key == pg.K_ESCAPE:
-                        # running = False
                         self.app.quit()
 
                 if event.type == pg.MOUSEBUTTONDOWN:
@@ -126,22 +112,26 @@ class MainMenuScreen(BaseScreen):
 
                             # Trigger button action
                             if button.text == "START GAME":
-                                print("Starting game...")
-                                # self.app.move_to(ScreenTypes.GAME)
-                                # running = False
+                                self.app.move_to(ScreenTypes.GAME)
+                                running = False
                             elif button.text == "VIEW HIGHSCORES":
                                 print("Opening highscores...")
                             elif button.text == "INSTRUCTIONS":
-                                print("Opening instructions...")
+                                self.app.move_to(ScreenTypes.INSTRUCTIONS)
+                                running = False
                             elif button.text == "EXIT":
-                                # running = False
                                 self.app.quit()
 
-            for button in buttons:
+            for i, button in enumerate(buttons):
                 button.update(mouse_pos)
+                if button.hovered:
+                    buttons[selected_index].selected = False
+                    selected_index = i
+                    buttons[selected_index].selected = True
+
 
             screen.fill("black")
-            title = font_large.render("PAC-MAN", True, "yellow")
+            title = pg.font.Font(None, 80).render("PAC-MAN", True, "yellow")
             title_rect = title.get_rect(center=(SCREEN_WIDTH//2, 80))
             screen.blit(title, title_rect)
 
