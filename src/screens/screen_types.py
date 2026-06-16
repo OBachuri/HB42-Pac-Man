006f9af7c4ccf558a2865aae3772efadc47122b2
Irek Iunusov <iiunusov@1-A-1.42heilbronn.de>
2026-06-16 12:57:12 +0200

    screen types enum

---
+++ src/screens/screen_types.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class ScreenTypes(Enum):
+    MAIN_MENU = "main_menu"
+    GAME = "game"
+    # PAUSE_MENU = "pause_menu"
+    # GAME_OVER = "game_over"
+    # VICTORY = "victory"
+    # INSTRUCTIONS = "instructions"
+    # HIGH_SCORES = "high_scores"
+    # TIME_LIMIT = "time_limit"
