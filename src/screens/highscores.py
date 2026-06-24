7c31e69696388492df5b663e738483570d32d24a
Irek Iunusov <iiunusov@1-D-1.42heilbronn.de>
2026-06-24 18:47:03 +0200

    showing place after storing a score

--- src/screens/highscores.py
+++ src/screens/highscores.py
@@ -13,13 +13,27 @@ class HighscoresScreen(BaseScreen):
         self.app = app
 
     async def run(self) -> None:
-        self.highscores = HighscoresHandler.get_highscores(self.app.config)
+        highscores = HighscoresHandler.get_highscores(self.app.config)
+
+        place_info = ""
+        if self.app.score_to_show >= 0:
+            score_vals = [list(d.values())[0] for d in highscores]
+            try:
+                place = score_vals.index(self.app.score_to_show, 0) + 1
+            except ValueError:
+                place = 11
+            if place > 10:
+                place_info = "Sorry, you are not in top 10"
+            else:
+                place_info = f"You are in {place} place"
 
         font = self.app.small_font
         title_font = self.app.large_font
         line_height = font.get_linesize() + 8
         title_line_height = title_font.get_linesize() + 8
         title_surf = title_font.render("Highscores:", False, "yellow")
+
+        place_inf_surf = font.render(place_info, False, "white")
 
         x = SCREEN_WIDTH // 2
 
@@ -40,13 +54,16 @@ class HighscoresScreen(BaseScreen):
             self.app.screen.blit(title_surf, (x - title_surf.width // 2, y))
             y += title_line_height * 2
 
-            if not self.highscores:
+            if not highscores:
                 score_surf = font.render(
                     "No results have been recorded so far.", False, "yellow")
                 self.app.screen.blit(score_surf,
                                      (x - score_surf.width // 2, y))
             else:
-                for nbr, highscore in enumerate(self.highscores, start=1):
+                if place_info:
+                    self.app.screen.blit(place_inf_surf, (x - place_inf_surf.width // 2, y))
+                    y += line_height * 1.5
+                for nbr, highscore in enumerate(highscores, start=1):
                     name, score = list(highscore.items())[0]
                     score_str = f"{nbr}. {name} - {score}"
                     score_surf = font.render(score_str, False, "yellow")
