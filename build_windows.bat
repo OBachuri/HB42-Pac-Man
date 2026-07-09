b2e03581a1bfc6f56c140640219c4fdce36a329e
Oleksandr Bachurin <obachuri@2-A-2.42heilbronn.de>
2026-07-09 18:11:42 +0300

    windows distr

--- build_windows.bat
+++ build_windows.bat
@@ -1,4 +1,5 @@
 ﻿
-pyinstaller -n "PacMan-42" --windowed -p src/ --add-data="src/inc:inc"  src/main.py
+pyinstaller -n "PacMan-42" --windowed  --onefile -p src/ --add-data="src/inc:inc"  src/main.py
 
-copy src\config_web.json dist\PacMan-42\
+copy src\config_web.json dist\
+copy src\ReadMe.txt dist\
---
+++ src/ReadMe.txt
@@ -0,0 +1,14 @@
+﻿PacMan 42
+
+Controls in game:
+W or ⇧ - Move Up
+A or ⇦ - Move Left
+S or ⇩ - Move Down
+D or ⇨ - Move Right
+Space - Stop
+Esc - Pause / Exit game
+F11 - Full screan
+
+Source:
+https://obachuriiiunusov.itch.io/pacman42
+https://github.com/OBachuri/HB42-Pac-Man
